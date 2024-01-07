534de196086b40fd9c387d6e4531f723bc5d39b8
James Williamson <150986379+AutoTechWebDev@users.noreply.github.com>
2024-01-07 17:19:14 -0500

    Delete PySUD directory

--- PySUD/PySUD_room.py
+++
@@ -1,7 +0,0 @@
-# ROOM INFORMATION BELOW HERE
-class Room:
-    def room(self, description, exits, items=None, monster=None):
-        self.description = description
-        self.exits = exits
-        self.items = items if items else []
-        self.monster = monster
